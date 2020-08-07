93ca4d57b66419723ba7c1702e248ec0f5274b5d
AyushB <66013664+ayushsb24@users.noreply.github.com>
2020-08-07 18:14:26 +0530

    Totake input

--- pythonfriend.py
+++ pythonfriend.py
@@ -4,9 +4,11 @@ happy = pyttsx3.init()
 voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 #sets Rates
 happy.setProperty('rate', 150) 
+
+txt=input("Enter Text : ")
 # Use female voice 
 happy.setProperty('voice', voice_id) 
-happy.say("Hello EveryBuddy I am Happy!")
+happy.say(txt)
 happy.say("")
 
 
