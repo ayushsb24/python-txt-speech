019af4b17255c81db0e8805bc4c5aa3762baa900
Prashans Dixit <71697179+prishuprograms@users.noreply.github.com>
2020-10-01 15:02:40 +0530

    Updating the code..

--- pythonfriend.py
+++ pythonfriend.py
@@ -1,16 +1,24 @@
+# Importing Modules
 import pyttsx3
+
 happy = pyttsx3.init()
 
+# Path
 voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+
 #sets Rates
 happy.setProperty('rate', 150) 
 
+# Input
 txt=input("Enter Text to Speak: ")
-# Use female voice 
+
+# Use female voice[0]
+# Use male voice [1]
 happy.setProperty('voice', voice_id) 
 happy.say(txt)
 happy.say("")
 
+# Helps to speak
 happy.runAndWait()
 
 
